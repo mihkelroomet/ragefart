e2ca3e64ce1d131df7f5a8677dd9eeab1559b059
Mihkel Roomet <mihkel.roomet@ut.ee>
2025-10-30 14:49:02 +0100

    stage0 now works. added logic for brush launch and trigger zones

--- Assets/Scripts/ToiletBrush.cs
+++ Assets/Scripts/ToiletBrush.cs
@@ -1,16 +1,67 @@
+using System.Collections;
 using UnityEngine;
 
 public class ToiletBrush : MonoBehaviour
 {
+    [SerializeField] float launchDelay;
+    [SerializeField] float accel = 60f;
+    [SerializeField] float maxSpeed = 12f;
+    [SerializeField] Transform target;
+
+    Vector2 _initialLocation;
+    Vector2 _moveDirection;
+    Vector2 _targetLocation;
+    bool _isMoving;
+    
     [SerializeField] LoseEvent loseEvent;
     
     [SerializeField] GameState gameState;
     
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _initialLocation = transform.position;
+        _targetLocation = target.transform.position;
+        _moveDirection = (_targetLocation - _initialLocation).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        if (!_isMoving) return;
+
+        // if past target
+        if (((Vector2)transform.position - _initialLocation).magnitude >= (_targetLocation - _initialLocation).magnitude)
+        {
+            _isMoving = false;
+            return;
+        }
+        
+        SetSpeed();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (gameState.gameIsRunning && other.CompareTag("Player"))
         {
             loseEvent.Raise(new Events.Lose());
         }
+    }
+
+    void SetSpeed()
+    {
+        _rb.linearVelocity = Vector2.MoveTowards(_rb.linearVelocity, _moveDirection * maxSpeed, accel * Time.fixedDeltaTime);
+    }
+
+    public void Launch()
+    {
+        StartCoroutine(LaunchCoroutine());
+    }
+
+    IEnumerator LaunchCoroutine()
+    {
+        yield return new WaitForSeconds(launchDelay);
+        _isMoving = true;
     }
 }
