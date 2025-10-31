8a76929cbb0dc8c0b72b7d479c6904046602cbc0
Mihkel Roomet <mihkel.roomet@ut.ee>
2025-10-31 22:22:44 +0100

    buttons no longer play hover sound again on click

--- Assets/Scripts/UI/UIButtonSFX.cs
+++ Assets/Scripts/UI/UIButtonSFX.cs
@@ -4,19 +4,13 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(Selectable))]
 public class UIButtonSFX : MonoBehaviour,
-    IPointerEnterHandler, IPointerClickHandler, ISelectHandler
+    IPointerEnterHandler, IPointerClickHandler
 {
     Selectable _sel;
 
     void Awake() => _sel = GetComponent<Selectable>();
 
     public void OnPointerEnter(PointerEventData e)
-    {
-        if (_sel.interactable) UIAudio.I?.Hover();
-    }
-
-    // Keyboard/gamepad focus
-    public void OnSelect(BaseEventData e)
     {
         if (_sel.interactable) UIAudio.I?.Hover();
     }
